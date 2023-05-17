bb550fbcddc5b092f950245311281c7fcbddd0ef
sai s <yupalreddy.b@gmail.com>
2023-05-18 02:44:49 +0530

    ui changes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
+import{defineCustomElements} from 'stencil-members-app/loader'
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  defineCustomElements(window)
